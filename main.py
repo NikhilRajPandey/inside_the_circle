f14397065c2c390f34ed9aa802d2e2c7d370fc8b
nikhilrajpandey <nikhilrajpandey1@gmail.com>
2019-12-29 12:26:02 +0530

    Program made

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import math
+
 # This Programs basically tells you that a point is in the circle or not
 
-def isinthecircle(radius,centre,other_point):
-    pass
+def isinsidethecircle(radius,centre,other_point):
+    # Absoulute Value is needed
+    length = abs(other_point[0]) - abs(centre[0])
+    breadth = abs(other_point[1]) - abs(centre[1])
+    distance_between_centre_and_point = math.sqrt((length*length)+(breadth*breadth))
+    if distance_between_centre_and_point > radius:
+        return False
+    else:
+        return True
